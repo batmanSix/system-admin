c7e74309c4c5364c93ad25cf84a7ee1a95ac9849
kangbo <841331654.com>
2022-07-06 22:41:57 +0800

    组件按需引入开发 配置main ts入口文件

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
 import { createApp } from 'vue'
+import { setupNaiveUI } from './plugins';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+async function setupApp() {
+
+  
+    const app = createApp(App);
+  
+
+
+  
+    // 按需引入naiveUI
+    setupNaiveUI(app);
+  
+  
+    // 路由准备就绪后挂载 App
+    app.mount('#app');
+  }
+  
+  setupApp();
---
+++ src/plugins/index.ts
@@ -0,0 +1,3 @@
+import setupNaiveUI from './naive';
+
+export { setupNaiveUI };
