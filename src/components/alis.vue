7416394eae85f7c961a10c6b2a405d57746c6eb4
kangbo <841331654.com>
2022-07-07 22:13:11 +0800

    vite别名配置

---
+++ src/components/alis.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>别名配置</div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        console.log(1231)
+    },
+})
+</script>
